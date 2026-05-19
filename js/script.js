97e11ca378255cb53a7dc39ab475388df739a41a
morasofiaa2004 <morasofiaa2004@gmail.com>
2026-05-19 16:41:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
+const pantalla = document.getElementById("pantalla");
 
+// bloque inicial
+crearBloque(pantalla);
+
+function crearBloque(contenedor) {
+
+  const bloque = document.createElement("div");
+  bloque.classList.add("bloque");
+
+  bloque.innerText = "click";
+
+  contenedor.appendChild(bloque);
+
+  bloque.addEventListener("click", dividir);
+}
+
+function dividir(e) {
+
+  e.stopPropagation();
+
+  const bloque = e.currentTarget;
+
+  // evita dividir infinitamente
+  if (bloque.dataset.dividido) return;
+
+  bloque.dataset.dividido = true;
+
+  bloque.innerHTML = "";
+
+  // decide división horizontal o vertical
+  const vertical = Math.random() > 0.5;
+
+  bloque.style.display = "flex";
+  bloque.style.flexDirection = vertical ? "row" : "column";
+
+  crearBloque(bloque);
+  crearBloque(bloque);
+}
